4e6286759765a6967589cb4e1e864a139bd396ee
Samson Gudise <samson.gudise@stem.com>
2021-07-09 09:26:59 -0600

    allow Bahrain in cloudfront

--- cloudfront/cloudfront.tf
+++ cloudfront/cloudfront.tf
@@ -104,7 +104,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE", "IN"]
+      locations        = ["US", "CA", "GB", "DE", "IN", "BH"]
     }
   }
 
